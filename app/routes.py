4e6b328d0d3203328c84c9f1ece478cd0d7e307c
awright222 <alex.gr.wright@gmail.com>
2024-12-10 14:39:28 -0700

    Show appointments on the main page

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,29 @@
+import os
+import sqlite3
 from flask import Blueprint, render_template
+from datetime import datetime
 
 bp = Blueprint('main', __name__, url_prefix='/')
+DB_FILE = os.environ.get("DB_FILE")
 
 @bp.route("/")
 def main():
-    return render_template('main.html')
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT id, name, start_datetime, end_datetime
+        FROM appointments
+        ORDER BY start_datetime;
+    """)
+    rows = cursor.fetchall()
+    conn.close()
+
+    # Convert datetime strings to datetime objects
+    appointments = []
+    for row in rows:
+        id, name, start_datetime, end_datetime = row
+        start_datetime = datetime.strptime(start_datetime, '%Y-%m-%d %H:%M:%S')
+        end_datetime = datetime.strptime(end_datetime, '%Y-%m-%d %H:%M:%S')
+        appointments.append((id, name, start_datetime, end_datetime))
+
+    return render_template("main.html", appointments=appointments)
