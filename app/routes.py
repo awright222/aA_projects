5cf04f9070345986874fe95cd5d70f0285402850
awright222 <alex.gr.wright@gmail.com>
2024-12-10 15:14:30 -0700

    handle form submission and insert new appointments

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 import os
 import sqlite3
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from datetime import datetime
-from .forms.py import AppointmentForm
+from .forms import AppointmentForm
 
 bp = Blueprint('main', __name__, url_prefix='/')
 DB_FILE = os.environ.get("DB_FILE")
@@ -10,6 +10,24 @@ DB_FILE = os.environ.get("DB_FILE")
 @bp.route("/", methods=["GET", "POST"])
 def main():
     form = AppointmentForm()
+    if form.validate_on_submit():
+        params = {
+            'name': form.name.data,
+            'start_datetime': datetime.combine(form.start_date.data, form.start_time.data),
+            'end_datetime': datetime.combine(form.end_date.data, form.end_time.data),
+            'description': form.description.data,
+            'private': form.private.data
+        }
+        conn = sqlite3.connect(DB_FILE)
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO appointments (name, start_datetime, end_datetime, description, private)
+            VALUES (:name, :start_datetime, :end_datetime, :description, :private)
+        """, params)
+        conn.commit()
+        conn.close()
+        return redirect(url_for('main.main'))
+
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
     cursor.execute("""
